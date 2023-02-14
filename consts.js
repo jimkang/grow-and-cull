49fb867d611f8f48377b64c4c662c86780770f6c
Jim Kang <jimkang@gmail.com>
2023-02-14 01:15:20 -0500

    Use cello sample.

--- consts.js
+++ consts.js
@@ -1,5 +1,6 @@
 export const defaultSecondsPerTick = 2;
 export const diamondLimit = 15;
-export var sampleFilenames = ['Vibraphone.sustain.ff.D4.wav'];
+//export var sampleFilenames = ['Vibraphone.sustain.ff.D4.wav'];
+export var sampleFilenames = ['cello-d2.wav'];
 export const ticksPerRiff = 6;
 export const repeatsPerRiff = 2.5;
